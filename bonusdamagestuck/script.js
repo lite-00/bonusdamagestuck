c144dabeb0342111561c54de694d3643463f4255
lite-00 <jcjessstar5799@gmail.com>
2022-02-12 05:07:30 -0500

    css and js base files, testing

---
+++ bonusdamagestuck/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.pre--multiline').forEach(block => hljs.highlightBlock(block));
+});
+
+function scrollToMessage(event, id) {
+    var element = document.getElementById('message-' + id);
+
+    if (element) {
+        event.preventDefault();
+
+        element.classList.add('chatlog__message--highlighted');
+
+        window.scrollTo({
+            top: element.getBoundingClientRect().top - document.body.getBoundingClientRect().top - (window.innerHeight / 2),
+            behavior: 'smooth'
+        });
+
+        window.setTimeout(function() {
+            element.classList.remove('chatlog__message--highlighted');
+        }, 2000);
+    }
+}
+
+function showSpoiler(event, element) {
+    if (element && element.classList.contains('spoiler-text--hidden')) {
+        event.preventDefault();
+        element.classList.remove('spoiler-text--hidden');
+    }
+    if (element && element.classList.contains('chatlog__attachment--hidden')) {
+        event.preventDefault();
+        element.classList.remove('chatlog__attachment--hidden');
+    }
+}
